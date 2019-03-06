8ac6db52ff3928dfb280523d33c57352ae36af00
Sachin Mahajan <sachinm78@gmail.com>
2019-03-06 17:42:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'pry'
 
 def load_library(path)
-  emoticons = YAML.load_file
+  emoticons = YAML.load_file(path)
   emoticon_hash = Hash.new
 
   emoticon_hash["get_emoticon"] = Hash.new
