5cc4dbe749144b38ef1798a5f6dc11fe4bccc3ce
Sachin Mahajan <sachinm78@gmail.com>
2019-03-06 17:53:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,9 +16,15 @@ def load_library(path)
 end
 
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
+
 
 def get_english_meaning
   # code goes here
